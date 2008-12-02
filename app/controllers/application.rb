1556f1c985ab7f74d3062b09478e1ece27f7d90b
M@ McCray <darthapo@gmail.com>
2008-12-02 12:03:34 -0600

    Added hoptoad error notification

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,6 +2,7 @@
 # The filters added to this controller will be run for all controllers in the application.
 # Likewise will all the methods added be available for all controllers.
 class ApplicationController < ActionController::Base
+  include HoptoadNotifier::Catcher
 
    include LoginSystem
    include AutoLogin
---
+++ config/initializers/hoptoad.rb
@@ -0,0 +1,3 @@
+HoptoadNotifier.configure do |config|
+  config.api_key = 'ba62c17ef30d2b954ed130568411ec1a'
+end
